c75ca2be1a17bc782110d452ed0f171e11f9af9d
PelykhD <dddddp.me@gmail.com>
2026-07-01 20:07:19 +0300

    lab4
    Added functionality to count and display the number of divisors of a natural number input by the user.

--- lab4.java
+++ lab4.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class lab4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Введіть натуральне число n: ");
+        
+        if (!scanner.hasNextInt()) return;
+        int n = scanner.nextInt();
+        
+        int count = 0;
+        
+        for (int r = 1; r * (r + 1) < n; r++) {
+            if (n % r == 0) {
+                count++;
+            }
+        }
+        
+        System.out.println("Кількість рівних дільників числа " + n + ": " + count);
+        
+        scanner.close();
+    }
+}
